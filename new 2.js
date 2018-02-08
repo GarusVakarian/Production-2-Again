ce5fface8f2ceca9b6ae4aac58e6833e7a6e73a6
GarusVakarian <34307820+GarusVakarian@users.noreply.github.com>
2018-02-08 11:10:59 -0500

    Add files via upload

---
+++ new 2.js
@@ -0,0 +1,6 @@
+var cavas = document.querySelector("canvas"); //link to the HTML
+
+canvas.width = 1000;// canvas set to 1000px width
+canvas.height = 800;// canvas set to 800px height
+
+var surface = canvas .getContext("2d"); // canvas is now set to a 2d surface 
